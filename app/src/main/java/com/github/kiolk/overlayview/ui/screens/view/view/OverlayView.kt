b210995cf7109a7fc4b593cb0d34f589ba03111b
Yauheni Slizh <tyteishi@gmail.com>
2024-12-12 11:31:26 +0100

    Added two stab element for future implementation of overlay

---
+++ app/src/main/java/com/github/kiolk/overlayview/ui/screens/view/view/OverlayView.kt
@@ -0,0 +1,54 @@
+package com.github.kiolk.overlayview.ui.screens.view.view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.drawable.Drawable
+import android.text.TextPaint
+import android.util.AttributeSet
+import android.view.View
+import com.github.kiolk.overlayview.R
+
+class OverlayView : View {
+
+    constructor(context: Context) : super(context) {
+        init(null, 0)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(attrs, 0)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(
+        context,
+        attrs,
+        defStyle
+    ) {
+        init(attrs, defStyle)
+    }
+
+    private fun init(attrs: AttributeSet?, defStyle: Int) {
+        // Load attributes
+        val a = context.obtainStyledAttributes(
+            attrs, R.styleable.OverlayView, defStyle, 0
+        )
+
+        a.recycle()
+
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        // TODO: consider storing these as member variables to reduce
+        // allocations per draw cycle.
+        val paddingLeft = paddingLeft
+        val paddingTop = paddingTop
+        val paddingRight = paddingRight
+        val paddingBottom = paddingBottom
+
+        val contentWidth = width - paddingLeft - paddingRight
+        val contentHeight = height - paddingTop - paddingBottom
+    }
+}
